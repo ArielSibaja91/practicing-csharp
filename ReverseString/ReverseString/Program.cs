7b8f5f367ee2dd48e49ece3ee26071b8a58e1461
Ariel Sibaja <asibaja91@gmail.com>
2025-07-02 09:36:04 -0600

    Reverse a string excercise

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        /*
+         * Ask user to input a message
+         * Print in order
+         * Print in reverse
+         */
+        static void Main(string[] args) 
+        {
+            Console.Write("Enter a message: ");
+            string? message = Console.ReadLine();
+
+            for (int i = 0; i < message?.Length; i++) 
+            { 
+                Console.WriteLine(message[i]);
+            }
+            Console.WriteLine();
+            for (int i = message.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(message[i]);
+            }
+        }
+    }
+}
