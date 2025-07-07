da1164b734feb9b1b554953ebc23fff1d9de5c0d
Ariel Sibaja <asibaja91@gmail.com>
2025-07-07 10:38:45 -0600

    Namespace and classes

--- PooCsharp/PooCsharp/Program.cs
+++ PooCsharp/PooCsharp/Program.cs
@@ -1,37 +1,6 @@
-﻿// First way to initialize an object based on a class
+﻿using PooCsharp.UFC;
+
 Fighter brawler = new Fighter("Ilia Topuria", 29, 17);
-// Second way to initialize an object
-var wrestler = new Fighter("Sean Brady", 32, 19);
 
 Console.WriteLine(brawler.Name);
-Console.WriteLine(wrestler.Name + " " + wrestler.Age + " years old " + wrestler.Fights + " fights");
 Console.WriteLine(brawler.BruceBuffer());
-public class Fighter
-{
-    private int _fights;
-    public string Name { get; set; }
-    public int Age { get; set; }
-    // Constructor method
-    public Fighter (string name, int age, int fights)
-    {
-        Name = name;
-        Age = age;
-        Fights = fights;
-    }
-    public int Fights
-    {
-        get { return _fights; }
-        set
-        {
-            if (value < 0)
-            {
-                value = 0;
-            }
-            _fights = value;
-        }
-    }
-    public string BruceBuffer()
-    {
-        return "It's timeeee....Presenting...." + Name + " of " + Age + " years old! And " + Fights + " fights!";
-    }
-}
---
+++ PooCsharp/PooCsharp/UFC/Fighter.cs
@@ -0,0 +1,32 @@
+﻿namespace PooCsharp.UFC
+{
+    public class Fighter
+    {
+        private int _fights;
+        public string Name { get; set; }
+        public int Age { get; set; }
+        // Constructor method
+        public Fighter(string name, int age, int fights)
+        {
+            Name = name;
+            Age = age;
+            Fights = fights;
+        }
+        public int Fights
+        {
+            get { return _fights; }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                _fights = value;
+            }
+        }
+        public string BruceBuffer()
+        {
+            return "It's timeeee....Presenting...." + Name + " of " + Age + " years old! And " + Fights + " fights!";
+        }
+    }
+}
